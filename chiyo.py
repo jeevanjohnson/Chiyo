7e2ff828b5e3c13e11d5b77e4d84d0b806e079e6
Cover <59410410+CoverOWO@users.noreply.github.com>
2020-08-07 13:03:22 -0700

    cool

--- chiyo.py
+++ chiyo.py
@@ -44,18 +44,19 @@ async def on_message(message):
 		artist = t.json()[0]['artist']
 		max_combo = t.json()[0]['max_combo']
 		bpm = t.json()[0]['bpm']
-		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
+		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Gatari: https://osu.gatari.pw/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
 		embed.set_author(name=f"{artist} - {title}", url=f"https://akatsuki.pw/b/{id_b}", icon_url=f"https://avatars0.githubusercontent.com/u/45724130?s=200&v=4.png")
 		embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{id_sb}/covers/cover.jpg')
 		await message.channel.send(embed=embed)
-	elif 'https://akatsuki.pw/b/' in message.content:
+	if 'https://akatsuki.pw/b/' in message.content:
 		c = message.content
 		b = c.replace('https://akatsuki.pw/b/','')
 		d = b.replace('?mode=0','')
 		e = d.replace('?mode=1','')
 		f = e.replace('?mode=2','')
+		fyusagfa = f.replace('-taiko','')
 		g = f.replace('?mode=3','')
-		t = requests.get(f'https://akatsuki.pw/api/get_beatmaps?limit=1&b={g}')
+		t = requests.get(f'https://akatsuki.pw/api/get_beatmaps?limit=1&b={g}&m=0')
 		if not t:
 			return
 		title = t.json()[0]['title']
@@ -64,7 +65,48 @@ async def on_message(message):
 		artist = t.json()[0]['artist']
 		max_combo = t.json()[0]['max_combo']
 		bpm = t.json()[0]['bpm']
-		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
+		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Gatari: https://osu.gatari.pw/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
+		embed.set_author(name=f"{artist} - {title}", url=f"https://akatsuki.pw/b/{id_b}", icon_url=f"https://avatars0.githubusercontent.com/u/45724130?s=200&v=4.png")
+		embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{id_sb}/covers/cover.jpg')
+		await message.channel.send(embed=embed)
+	if 'https://akatsuki.pw/d/' in message.content:
+		c = message.content
+		b = c.replace('https://akatsuki.pw/d/','')
+		d = b.replace('?mode=0','')
+		e = d.replace('?mode=1','')
+		f = e.replace('?mode=2','')
+		g = f.replace('?mode=3','')
+		t = requests.get(f'https://akatsuki.pw/api/get_beatmaps?limit=1&s={g}&m=0')
+		if not t:
+			return
+		title = t.json()[0]['title']
+		id_b = t.json()[0]['beatmap_id']
+		id_sb = t.json()[0]['beatmapset_id']
+		artist = t.json()[0]['artist']
+		max_combo = t.json()[0]['max_combo']
+		bpm = t.json()[0]['bpm']
+		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Gatari: https://osu.gatari.pw/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
+		embed.set_author(name=f"{artist} - {title}", url=f"https://akatsuki.pw/b/{id_b}", icon_url=f"https://avatars0.githubusercontent.com/u/45724130?s=200&v=4.png")
+		embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{id_sb}/covers/cover.jpg')
+		await message.channel.send(embed=embed)
+	if 'https://akatsuki.pw/d/' in message.content and '-taiko' in message.content:
+		c = message.content
+		b = c.replace('https://akatsuki.pw/d/','')
+		d = b.replace('?mode=0','')
+		e = d.replace('?mode=1','')
+		f = e.replace('?mode=2','')
+		ysfusf = f.replace('-taiko','')
+		g = ysfusf.replace('?mode=3','')
+		t = requests.get(f'https://akatsuki.pw/api/get_beatmaps?limit=1&s={g}&m=1')
+		if not t:
+			return
+		title = t.json()[0]['title']
+		id_b = t.json()[0]['beatmap_id']
+		id_sb = t.json()[0]['beatmapset_id']
+		artist = t.json()[0]['artist']
+		max_combo = t.json()[0]['max_combo']
+		bpm = t.json()[0]['bpm']
+		embed=discord.Embed(description=f'▸ Bloodcat: https://bloodcat.com/osu/{id_b} \n▸ Old Osu: https://old.ppy.sh/s/{id_sb}\n▸ Osu: https://osu.ppy.sh/b/{id_b} \n▸ Gatari: https://osu.gatari.pw/b/{id_b} \n▸ Max Combo: {max_combo} \n▸ BPM: {bpm}', color=0xb6ebf1)
 		embed.set_author(name=f"{artist} - {title}", url=f"https://akatsuki.pw/b/{id_b}", icon_url=f"https://avatars0.githubusercontent.com/u/45724130?s=200&v=4.png")
 		embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{id_sb}/covers/cover.jpg')
 		await message.channel.send(embed=embed)
