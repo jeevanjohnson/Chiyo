da04020606617cf8868849e86b2f879eb27a0c1c
Cover <59410410+coverosu@users.noreply.github.com>
2020-09-24 01:31:00 -0700

    shorter

--- chiyo.py
+++ chiyo.py
@@ -1,9 +1,5 @@
-import discord
-from discord.ext import commands
-from config import token, prefix
+from config import token
 from events import Chiyo
 
-client = commands.Bot(command_prefix = prefix, case_insensitive=True)
-
-chiyo = Chiyo(token, client)
+chiyo = Chiyo(token)
 chiyo.run()
