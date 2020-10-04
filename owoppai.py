8d7841d0c35d62bcf447a5d5bc12ab431b9c104a
CoverOWO <ImEasyCJ@gmail.com>
2020-10-04 12:39:54 -0700

    dn part 2

--- owoppai.py
+++ owoppai.py
@@ -7,14 +7,13 @@ os.chdir(oppai_dir)
 
 acc = [100, 99, 98, 97, 96]
 
-def oppai(beatmap_id):
+def oppai(beatmap_id, mods = None, accc = None):
 	
 	command = []
 	ending_acc = []
 	command.append(f'./oppai {map_dir}/{beatmap_id}.osu')
-	for accuracy in acc:
-
-		command.append(f'{accuracy}%')
+	if accc:
+		command.append(f'{accc}%' if not '%' in accc else f'{accc}')
 		command.append('-ojson')
 
 		process = run(
@@ -22,6 +21,22 @@ def oppai(beatmap_id):
 		shell = True, stdout = PIPE, stderr = PIPE)
 		
 		output = loads(process.stdout.decode('utf-8', errors='ignore'))
-		ending_acc.append('{Acc}%: {PP}PP'.format(PP=round(output["pp"], 2), Acc=accuracy))
-		command.remove(f'{accuracy}%')
+		ending_acc.append('{Acc}%: {PP}PP'.format(PP=round(output["pp"], 2), Acc=accc))
+	else:
+		for accuracy in acc:
+
+			if mods:
+				command.append(f'+{mods}')
+
+			command.append(f'{accuracy}%')
+			
+			command.append('-ojson')
+
+			process = run(
+			' '.join(command),
+			shell = True, stdout = PIPE, stderr = PIPE)
+			
+			output = loads(process.stdout.decode('utf-8', errors='ignore'))
+			ending_acc.append('{Acc}%: {PP}PP'.format(PP=round(output["pp"], 2), Acc=accuracy))
+			command.remove(f'{accuracy}%')
 	return ending_acc
